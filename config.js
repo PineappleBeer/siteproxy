7494145ce2d0a1ecb63d4fe4adffde3131b31420
PineappleBeer <31506259+PineappleBeer@users.noreply.github.com>
2020-06-17 09:15:58 +0800

    mine config

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.com',
+    serverName: 'siteproxy.hoopecn.vercel.app',
 }
 let blockedSites = ['merlinblog.xyz']
 
