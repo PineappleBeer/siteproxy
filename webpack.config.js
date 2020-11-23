31d6b8804bd245cabea2e8900ad67e4b732aee6f
zym <zym@cetc28.com>
2020-11-23 09:41:13 +0800

    修改输出文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+/*
+ * @Author: zym
+ * @Date: 2020-11-20 16:42:17
+ * @LastEditors: zym
+ * @LastEditTime: 2020-11-23 09:38:25
+ * @Description: edit
+ */
 const path = require('path')
 module.exports = {
   entry: './cf_index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, './build'),
     filename: 'worker.js',
   }
 }
